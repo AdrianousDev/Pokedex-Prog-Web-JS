95ec44d2eff311003b81dc1d2946069abb6445b1
AdrianousDev <brunoadrianousz@gmail.com>
2025-10-19 22:21:00 -0300

    ajustes cardPrincipal (modularização e possíveis casos dentro do módulo)

--- js/modules/cardPrincipal.js
+++ js/modules/cardPrincipal.js
@@ -5,32 +5,112 @@ export default async function cardPrincipal(id) {
     `https://pokeapi.co/api/v2/pokemon/${id}/`
   );
 
-  // referencias elementos
+  updatePokemonImage(pokemonDados);
+
+  updatePokemonId(pokemonDados);
+
+  updatePokemonDescription(id);
+
+  updatePokemonDimensions(pokemonDados);
+
+  updatePokemonBaseExp(pokemonDados);
+
+  updatePokemonStats(pokemonDados);
+}
+
+function updatePokemonImage(pokemonDados) {
   const pokemonImg = document.querySelector("#pokemonImg");
-  const pokemonId = document.querySelector("#pokemonId");
-  const pokemonDescription = document.querySelector("#pokemonDescription");
-  const pokemonHeight = document.querySelector("#pokemonHeight");
-  const pokemonWeight = document.querySelector("#pokemonWeight");
-  const pokemonBaseExp = document.querySelector("#pokemonBaseExp");
-  const pokemonDivStats = document.querySelector("#pokemonDivStats");
+
+  if (!pokemonImg) {
+    return;
+  }
+
+  if (!pokemonDados.sprites?.front_default) {
+    pokemonImg.src = "./images/not-found.png";
+    return;
+  }
 
   pokemonImg.src = pokemonDados.sprites.front_default;
-  pokemonId.textContent = `#${pokemonDados.id}`;
+}
 
+function updatePokemonId(pokemonDados) {
+  const pokemonId = document.querySelector("#pokemonId");
+
+  const idFormatado = pokemonDados.id.toString().padStart(4, "0");
+
+  pokemonId.textContent = `#${idFormatado}`;
+}
+
+async function updatePokemonDescription(id) {
   // fetch no endpoint que tem os flavors
   const dadosDescription =
     await chamarApi(`https://pokeapi.co/api/v2/pokemon-species/${id}/
 `);
 
+  const pokemonDescription = document.querySelector("#pokemonDescription");
+
+  if (!pokemonDescription) {
+    return;
+  }
+
+  if (!dadosDescription) {
+    pokemonDescription.textContent = "Descrição Padrão.";
+    return;
+  }
+
   // preocura o flavor que está em "en"
   const enDescription = dadosDescription.flavor_text_entries.find(
     (item) => item.language.name == "en"
   );
-  pokemonDescription.textContent = enDescription.flavor_text;
+
+  // regex para tirar \n \f \r
+  const descriptionTratada = enDescription.flavor_text
+    .replace(/[\f\n\r]/g, " ")
+    .toLowerCase();
+
+  pokemonDescription.textContent = descriptionTratada;
+}
+
+function updatePokemonDimensions(pokemonDados) {
+  const pokemonHeight = document.querySelector("#pokemonHeight");
+  const pokemonWeight = document.querySelector("#pokemonWeight");
+
+  if (!pokemonHeight || !pokemonWeight) {
+    return;
+  }
+
+  if (!pokemonDados) {
+    return;
+  }
 
   pokemonHeight.textContent = `${pokemonDados.height / 10}m`;
   pokemonWeight.textContent = `${pokemonDados.weight / 10}kg`;
+}
+
+function updatePokemonBaseExp(pokemonDados) {
+  const pokemonBaseExp = document.querySelector("#pokemonBaseExp");
+
+  if (!pokemonBaseExp) {
+    return;
+  }
+
+  if (!pokemonDados) {
+    return;
+  }
+
   pokemonBaseExp.textContent = pokemonDados.base_experience;
+}
+
+function updatePokemonStats(pokemonDados) {
+  const pokemonDivStats = document.querySelector("#pokemonDivStats");
+
+  if (!pokemonDados) {
+    return;
+  }
+
+  if (!pokemonDivStats) {
+    return;
+  }
 
   pokemonDivStats.innerHTML = "";
   pokemonDados.stats.forEach((status) => {
